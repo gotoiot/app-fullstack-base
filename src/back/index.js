d32cfc6aecffa42c66f9686553c5d8b190458712
Agustin <jagustinbassi@gmail.com>
2020-07-24 15:37:14 -0300

    Moves static files into frontend dir and add feature to serve static files

--- src/back/index.js
+++ src/back/index.js
@@ -14,20 +14,20 @@ var express = require('express');
 var app     = express();
 var mysql   = require('./mysql-connector');
 
-// para parsear application/json
+// to parse application/json
 app.use(express.json()); 
-// para servir archivos estaticos
-app.use(express.static('.')); 
+// to serve static files
+app.use(express.static('/home/node/app/static/'));
 
 //=======[ Main module code ]==================================================
 
-app.get('/', function(req, res, next) {
+app.get('/devices/', function(req, res, next) {
     response = "{ 'key1':'value1' }"
     res.send(JSON.stringify(response)).status(200);
 });
 
 app.listen(PORT, function(req, res) {
-    console.log("NodeJS API working on port: " + PORT);
+    console.log("NodeJS API running correctly");
 });
 
 //=======[ End of file ]=======================================================
