da6a703465cc1ba37d992f3d388577b6b0f26537
Agustin <jagustinbassi@gmail.com>
2021-05-03 17:46:20 -0300

    Adds TS sample code

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,6 @@
 
 function SayHello(){
     let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!" + "\n" + current_value.value;
+    let new_value = "Hello world!!!" + "\n" + current_value.value;
     document.getElementById("textarea_1").innerHTML = new_value;
 }
